a82be81965b052f1e25bb1dbb1951b614663d758
qianzhong516 <zhongqian516@gmail.com>
2024-08-21 12:22:52 +0200

    add toggle slider

---
+++ src/ToggleSlider.jsx
@@ -0,0 +1,16 @@
+import { twMerge } from 'tailwind-merge';
+
+const ToggleSlider = ({ on, onToggle }) => {
+    const nobClasses = 'before:absolute before:content-[""] before:top-1/2 before:-translate-y-1/2 before:left-1 before:w-[15px] before:h-[15px] before:bg-white before:rounded-full before:transition-all before:duration-400';
+
+    const nobCheckedClasses = 'peer-checked:bg-indigo-700 before:peer-checked:left-full before:peer-checked:-translate-x-[calc(100%+4px)]';
+
+    return (
+        <label className='relative w-[44px] h-[24px] cursor-pointer'>
+            <input type="checkbox" checked={on} onChange={onToggle} className='peer opacity-0' />
+            <span className={twMerge('absolute inset-0 bg-gray-100 rounded-xl transition duration-400', nobClasses, nobCheckedClasses)}></span>
+        </label>
+    );
+}
+
+export default ToggleSlider;
